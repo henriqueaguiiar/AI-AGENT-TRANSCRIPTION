4d41a236d2a23aa7614c14196c3d7a31f6efa66c
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-04-22 19:15:36 -0300

    Gerando arquivo de legenda passando ocntexto de transcrição com prompt

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ load_dotenv()
 
 cliente  = OpenAI()
 
+prompt = "Esse audio é de um anuncio da jornada full-stack da hashtag, um conteúdo completaçõ sobre desenvolvimento"
+
 with open("audio.mp3", "rb") as arquivo:
     transcricao = cliente.audio.transcriptions.create(
         file=arquivo,
         model="whisper-1",
-        language="pt"
+        language="pt",
+        response_format="srt",
+        prompt=prompt
     )
 
 print(transcricao)
+
+with open("legenda.srt", "w", encoding="utf-8") as arquivo_legenda:
+    arquivo_legenda.write(transcricao)
