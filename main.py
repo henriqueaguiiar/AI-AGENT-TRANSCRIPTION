c4a72436669ff108d566530de0f882a28d63841d
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-04-22 20:36:15 -0300

    Criado interface com streamlit e refatoração do código em funções e chamadas dinamicas

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
-from openai import OpenAI
-from dotenv import load_dotenv
-from pydub import AudioSegment
+from legenda import legendar
+import streamlit as st
 
-arquivo_selecionado = "ja começou a jornada full stack.mp4"
-extensao_arquivo = arquivo_selecionado.split(".")[1]
-audio = AudioSegment.from_file(file=arquivo_selecionado, format=extensao_arquivo)
+#legenda = legendar("ja começou a jornada full stack.mp4", prompt = "Esse audio é de um anuncio da jornada full-stack da hashtag, um conteúdo completaçõ sobre desenvolvimento")
+ 
 
-audio.export("audio.mp3", format="mp3")
+def app():
+    st.header("Legendador", divider=True)
+    st.markdown("#### Gere a legenda de qualquer áudio ou vídeo automaticamente.")
+    
+    prompt = st.text_input("Digite o contexto do video para ajudar na legenda")
+    arquivo = st.file_uploader("Selecione um vídeo.pm4 ou áudio.mp3 para legendar", type=["mp4", "mp3"])
+    if arquivo:
+        legenda = legendar(arquivo, prompt)
+        st.write(f"Arquivo {arquivo.name} legendado com sucesso")
+        st.write(legenda)
 
-load_dotenv()
-
-cliente  = OpenAI()
-
-prompt = "Esse audio é de um anuncio da jornada full-stack da hashtag, um conteúdo completaçõ sobre desenvolvimento"
-
-with open("audio.mp3", "rb") as arquivo:
-    transcricao = cliente.audio.transcriptions.create(
-        file=arquivo,
-        model="whisper-1",
-        language="pt",
-        response_format="srt",
-        prompt=prompt
-    )
-
-print(transcricao)
-
-with open("legenda.srt", "w", encoding="utf-8") as arquivo_legenda:
-    arquivo_legenda.write(transcricao)
+if __name__ =="__main__":
+    app()
